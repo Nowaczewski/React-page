d9df6d4be6f0f4a0403449c2b38e007de31d260a
cheddar <nowaczewskijack@gmail.com>
2023-03-20 15:50:00 +0100

    add notation to index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/css/bootstrap.rtl.min.css";
 
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
+// import slick carousel
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
